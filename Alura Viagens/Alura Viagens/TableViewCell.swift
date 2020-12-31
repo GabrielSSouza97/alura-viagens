1109e79f8653690020e1a5df6107097a2f248d24
GabrielSSouza97 <gabriel.s.souza9755@gmail.com>
2020-12-31 14:20:16 -0300

    Módulo 2.1.3
    Formatando as células do TableView. Referenciando os elementos no TableViewCell, e atribuindo os devidos parâmetros no ViewController

---
+++ Alura Viagens/Alura Viagens/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  Alura Viagens
+//
+//  Created by Gabriel Santos Souza on 31/12/20.
+//  Copyright © 2020 Gabriel Santos Souza. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+}
--- Alura Viagens/Alura Viagens/ViewController.swift
+++ Alura Viagens/Alura Viagens/ViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tabelaViagens: UITableView!
+    @IBOutlet weak var viewHoteis: UIView!
+    @IBOutlet weak var viewPacotes: UIView!
     
-    let listaViagens: Array<String> = ["Rio de Janeiro", "Ceará", "São Paulo"]
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabelaViagens.dataSource = self
+        self.tabelaViagens.delegate = self
+        self.viewHoteis.layer.cornerRadius = 10
+        self.viewPacotes.layer.cornerRadius = 10
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,10 +29,18 @@ class ViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = listaViagens[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+        let viagemAtual = listaViagens[indexPath.row]
+        cell.labelTitulo.text = viagemAtual.titulo
+        cell.labelQuantidadeDias.text = "\(viagemAtual.quantidadeDias) dias"
+        cell.labelPreco.text = viagemAtual.preco
+        cell.imagemViagem.image = UIImage(named: viagemAtual.caminhoImagem)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 175
     }
 
 }
